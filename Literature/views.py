3676ea23def8f7be0753785be377d82bf7a089c3
sivqnov <sbolshakov02@gmail.com>
2023-05-24 11:30:19 +0300

    done

--- Literature/views.py
+++ Literature/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from Main.symbols import *
 # Create your views here.
 
-ITEMS_ON_PAGE = 1
+ITEMS_ON_PAGE = 4
 
 def writers(request):
     authors = Author.objects.order_by('name')
@@ -30,9 +30,8 @@ def view_author(request, author_id):
     return render(request, 'author.html', context)
 
 def genres(request):
-    items = Genre.objects.order_by('-id')
-    items_on_page = 6
-    pag = Paginator(items, items_on_page)
+    items = Genre.objects.order_by('title')
+    pag = Paginator(items, ITEMS_ON_PAGE)
     page_number = request.GET.get('page', 1)
     page = pag.get_page(page_number)
     context = {
@@ -50,9 +49,8 @@ def view_genre(request, genre_id):
     return render(request, 'genre.html', context)
 
 def works(request):
-    items = Work.objects.order_by('-id')
-    items_on_page = 6
-    pag = Paginator(items, items_on_page)
+    items = Work.objects.order_by('title')
+    pag = Paginator(items, ITEMS_ON_PAGE)
     page_number = request.GET.get('page', 1)
     page = pag.get_page(page_number)
     context = {
@@ -70,7 +68,6 @@ def view_work(request, work_id):
     return render(request, 'work.html', context)
 
 def get_writers(request):
-    profile = Profile.objects.get(user=request.user)
     array = ['name', '-name', 'date_of_birth', '-date_of_birth',]
     if request.method == "GET":
         try:
@@ -102,13 +99,11 @@ def get_writers(request):
         page = pag.get_page(page_number)
         context = {
             'items': page,
-            'profile': profile,
         }
         return render(request, 'inc/authors.html', context)
 
 def get_genres(request):
-    profile = Profile.objects.get(user=request.user)
-    array = ['name', '-name', 'date_of_birth', '-date_of_birth',]
+    array = ['title', '-title',]
     if request.method == "GET":
         try:
             cat = int(request.GET.get('category', 0))
@@ -118,17 +113,14 @@ def get_genres(request):
             cat=0
         q = clean_q(request.GET.get('q', '')[:MAX_QUERY_LENGTH])
         with_photo = request.GET.get('with_photo', 'off')
-        with_alias = request.GET.get('with_alias', 'off')
         page_number = int(request.GET.get('page', 1))
         page_action = request.GET.get('pag', 'blin')
         if q=='':
-            items = Author.objects.order_by(array[cat])
+            items = Genre.objects.order_by(array[cat])
         else:
-            items = Author.objects.filter(Q(name__iregex=q) | Q(alias__iregex=q) | Q(preview__iregex=q) | Q(content__iregex=q)).order_by(array[cat])
+            items = Genre.objects.filter(Q(title__iregex=q) | Q(preview__iregex=q) | Q(content__iregex=q)).order_by(array[cat])
         if with_photo == 'on':
             items = items.exclude(photo='')
-        if with_alias == 'on':
-            items = items.exclude(alias='')
         pag = Paginator(items, ITEMS_ON_PAGE)
         page = pag.get_page(page_number)
         if page_action == 'next' and page.has_next():
@@ -139,6 +131,37 @@ def get_genres(request):
         page = pag.get_page(page_number)
         context = {
             'items': page,
-            'profile': profile,
         }
-        return render(request, 'inc/authors.html', context)
+        return render(request, 'inc/genres.html', context)
+
+def get_works(request):
+    array = ['title', '-title',]
+    if request.method == "GET":
+        try:
+            cat = int(request.GET.get('category', 0))
+        except:
+            cat = 0
+        if cat >= len(array) or cat < 0:
+            cat=0
+        q = clean_q(request.GET.get('q', '')[:MAX_QUERY_LENGTH])
+        with_photo = request.GET.get('with_photo', 'off')
+        page_number = int(request.GET.get('page', 1))
+        page_action = request.GET.get('pag', 'blin')
+        if q=='':
+            items = Work.objects.order_by(array[cat])
+        else:
+            items = Work.objects.filter(Q(title__iregex=q) | Q(preview__iregex=q) | Q(content__iregex=q)).order_by(array[cat])
+        if with_photo == 'on':
+            items = items.exclude(photo='')
+        pag = Paginator(items, ITEMS_ON_PAGE)
+        page = pag.get_page(page_number)
+        if page_action == 'next' and page.has_next():
+            page_number += 1
+        elif page_action == 'prev' and page.has_previous():
+            page_number -= 1
+        pag = Paginator(items, ITEMS_ON_PAGE)
+        page = pag.get_page(page_number)
+        context = {
+            'items': page,
+        }
+        return render(request, 'inc/works.html', context)
